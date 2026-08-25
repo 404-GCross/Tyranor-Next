931891c79118109283ab61d01059c28806144616
Weiss-UltimateSavior <weiss450006@gmail.com>
2026-08-25 17:42:47 +0800

    build(app): bump versionName to 1.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.16"
+        versionName = "1.17"
         buildConfigField("String", "HIKARINAGI_CLIENT_ID", hikarinagiClientId.asBuildConfigString())
     }
 
