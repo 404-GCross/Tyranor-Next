403678b7a2595597a50aea8968ff1a48b2956bdd
Weiss-UltimateSavior <weiss450006@gmail.com>
2026-08-27 21:24:28 +0800

    build(app): bump versionName to 1.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,7 +103,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.20"
+        versionName = "1.21"
         buildConfigField("String", "HIKARINAGI_CLIENT_ID", hikarinagiClientId.asBuildConfigString())
     }
 
